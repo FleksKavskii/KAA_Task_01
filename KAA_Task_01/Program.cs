2431d9ca2dfb19439f7341081fbc9327077ac0f7
FleksKavskii <66283133+FleksKavskii@users.noreply.github.com>
2020-11-16 17:51:10 +0300

    KAA_Task_01
    Задача 1
    
    Легенда:
    
    Вы приходите в магазин и хотите купить за своё золото кристаллы. В вашем кошельке есть какое-то количество золота, продавец спрашивает у вас, сколько кристаллов вы хотите купить? После сделки у вас остаётся какое-то количество золота и появляется какое-то количество кристаллов.
    
    Формально:
    
    При старте программы пользователь вводит начальное количество золота. Потом ему предлагается купить какое-то количество кристаллов по цене N (задать в программе самому). Пользователь вводит число и его золото конвертируется в кристаллы. Остаток золота и кристаллов выводится на экран. Если сделку невозможно совершить, то выводятся данные, актуальные перед сделкой.
    Дополнительное условие:
    
    Следует решить данную задачу без использования условного оператора

---
+++ KAA_Task_01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KAA_Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coins;
+            int price = 15;
+            int numberOfcrystals;
+            Console.Write("Введите колличество монет: ");
+            coins = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Стоимость одного кристалла {price} монет. Сколько желаете купить?: ");
+            numberOfcrystals = Convert.ToInt32(Console.ReadLine());
+            if ((numberOfcrystals * price) <= coins)
+                coins = coins - (numberOfcrystals * price);
+            else
+                numberOfcrystals = 0;
+            Console.WriteLine($"У Вас в сумке осталось {coins} монет и появилось {numberOfcrystals} кристаллов");
+
+
+        }
+    }
+}
